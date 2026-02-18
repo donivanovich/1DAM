c037d3c9c5f5cdbef5de1e96ede23c035eae01e8
donivanovich <kosoivan12@gmail.com>
2026-02-18 14:40:45 +0100

    Moving Folders

--- shawarmas/web/shawar+/quitar_del_carrito.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-$id_producto = intval($_POST['id_producto']);
-if (isset($_SESSION['carrito'][$id_producto])) {
-    unset($_SESSION['carrito'][$id_producto]);
-}
-header("Location: proceso_carrito.php");
-exit();
-?>
